b8815040cf73c7701cd9b09802ccc20697aa01a1
paikjosh <junhapaik@gmail.com>
2025-05-13 14:29:07 -0400

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -8,7 +8,7 @@ import utility_functions as uf
 
 # Requires: All other columns except for a column that contains data should either be in type float or int.
 # Modifies: target_file.
-# Effects: Show average and standard deviation of all columns except for the date column
+# Effects: Shows average and standard deviation of all columns except for the date column
 def stat_measures(target_file, target_file_name, date_col_name):
     print('Statistical measures for ', target_file_name,':', sep='', end='\n')
     non_date_col_list = target_file.columns.values.tolist()
@@ -25,7 +25,7 @@ def stat_measures(target_file, target_file_name, date_col_name):
 
 # Requires: All other columns except for a column that contains data should either be in type float or int.
 # Modifies: None.
-# Effects: Build ARIMA and graph the result.
+# Effects: Builds ARIMA and graph the result.
 def arima(target_file,target_col_name, steps):
     from statsmodels.tsa.arima.model import ARIMA
     from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
@@ -33,24 +33,24 @@ def arima(target_file,target_col_name, steps):
 
     print('ARIMA:', end='\n')
 
-    # running Augmented Dickey-Fuller test to determine d value
+    # Running Augmented Dickey-Fuller test to determine d value
     print('Augmented Dickey-Fuller (ADF) test using significant level (alpha) of 0.05: ')
     temp_data = target_file[target_col_name].copy()
     d = 0
     while True:
         adf_test_result = adfuller(temp_data.dropna())
         if adf_test_result[1] < 0.05:
-            # data is stationary
+            # Data is stationary
             print('data is stationary at d =', d, end='\n')
             print('This d value will be used for ARIMA', end='\n\n')
             break
         else:
-            # data is non stationary
+            # Data is non stationary
             temp_data = temp_data.diff()
             d += 1
 
-    # plotting PACF and ACF to determine q and p
-    # setting lags for PACF and ACF to be at most 50% of sample size of target_file
+    # Plotting PACF and ACF to determine q and p
+    # Setting lags for PACF and ACF to be at most 50% of sample size of target_file
     tf_lags = math.floor(0.5 * len(target_file))
     plot_pacf(target_file[target_col_name], lags=tf_lags)
     plt.title('PACF')
@@ -65,20 +65,20 @@ def arima(target_file,target_col_name, steps):
     print('Type p first, then q. They must be seperated by a comma', end='\n')
     pdq_input = uf.input_indices()
 
-    # fit ARIMA model
+    # Fitting ARIMA model
     model = ARIMA(target_file[target_col_name], order=(pdq_input[0], d, pdq_input[1]))
     result = model.fit()
 
     print(result.summary())
 
-    # predictions
+    # Making predictions
     predictions = result.get_forecast(steps=steps)
     predicted_values = predictions.predicted_mean
 
     print('ARIMA results:', end='\n')
     print(predicted_values)
 
-    # graphing ARIMA results
+    # Graphing ARIMA results
     plt.plot(target_file[target_col_name], label='Observed')
     plt.plot(predicted_values, label='Predicted')
     plt.xlabel('time t (t = 0 is base time)')
@@ -102,11 +102,11 @@ def var(target_file, steps):
     result = model.fit(ic='aic')
     print('Lag order:', result.k_ar)
 
-    # fitting
+    # Fitting
     fitted_model = model.fit(result.k_ar)
     print(fitted_model.summary())
 
-    # predictions
+    # Making predictions
     lag = target_file.values[-result.k_ar:]
     predictions = fitted_model.forecast(y=lag, steps=steps)
     predictions_index = range(len(target_file), len(target_file) + steps)
@@ -116,7 +116,7 @@ def var(target_file, steps):
     print(predictions_df, end='\n')
     print('Note that index above represents time, where index 0 is the base period', end = '\n\n')
 
-    # graphing results
+    # Graphing results
     for i in target_file.columns.values.tolist():
         plt.plot(target_file[i], label=i + ' Observed')
         plt.plot(predictions_df[i], label=i + ' Predicted')
